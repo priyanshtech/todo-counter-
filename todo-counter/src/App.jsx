3800fde3691c4cef3caeb9e861ad6be97a710485
Priyansh Khatri <priyanshkhatri8690@gmail.com>
2025-12-16 21:46:50 +0530

    added style to it

--- todo-counter/src/App.jsx
+++ todo-counter/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
       className={`min-h-screen flex flex-col items-center gap-10 py-12 
       ${theme === "light" ? "bg-white text-black" : "bg-black text-white"}`}
     ><h1 className="text-5xl font-bold mb-8">TASK MANAGER</h1>
-    <button onClick={handleNum}>increase me {num}</button>
+    <button onClick={handleNum} className="border-2 border-2 px-2 rounded-lg">increase me {num}</button>
       <ThemeToggle/>
       <Counter/>
       <Todo/>
